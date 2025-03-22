aeecad551a71303977f2131fd4ca6a9768eaa3c0
Matthew Evans <git@ml-evs.science>
2025-03-22 13:54:34 +0000

    Add README note about mirror status and update setup.py/CI
    Add README note about mirror status and update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
     readme = f.read()
 
 setup(
-    name="galvani",
+    name="galvani-mirror",
     version="0.4.1",
     description="Open and process battery charger log data files",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/echemdata/galvani",
+    url="https://github.com/datalab-org/galvani-mirror",
     author="Chris Kerr",
     author_email="chris.kerr@mykolab.ch",
     license="GPLv3+",
